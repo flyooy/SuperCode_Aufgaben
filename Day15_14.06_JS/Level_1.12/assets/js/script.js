3daec6940cf75977537710c0aae0b714b0cde1ea
flyooy <flyooy@gmail.com>
2024-06-14 12:57:57 +0200

    JS Level

---
+++ Day15_14.06_JS/Level_1.12/assets/js/script.js
@@ -0,0 +1,23 @@
+ let score1 = 5 + 5 * 10;  
+console.log('Ergebnis: ' + score1);
+ 
+let score2 = (5 + 5) * 10;  
+console.log('Ergebnis: ' + score2);
+// Du kannst Werte auf Variablen addieren:  
+let score = 0;  
+score = score + 10;  
+console.log('Ergebnis: ' + score);
+//Hiervon gibt es eine verkürzte Version:  
+score += 10;  
+console.log('Ergebnis: ' + score)
+//Nun probiere es hiermit:
+//Inkrementierungen und Dekrementierungen können wie folgt verwendet werden:  
+let zahl = 1;  
+zahl = zahl + 1;  
+zahl += 1;
+//Verwende den Inkrement-Operator:  
+zahl++;  
+console.log('increment: ' + zahl);
+//Verwende den Dekrement-Operator:  
+zahl--;  
+console.log('dekrement: ' + zahl);
---
+++ Day15_14.06_JS/Level_1.7/assets/js/script.js
@@ -0,0 +1,11 @@
+let helloWorld = "Hello World";
+console.log(helloWorld);
+
+const pi = 3.14;
+//pi = 2;
+
+let burjDubai = 828;
+let fullName = "Jan Schmidt";
+const eifelTower = 324;
+const camelCase = "camelCase";
+let kursStatus = true;
